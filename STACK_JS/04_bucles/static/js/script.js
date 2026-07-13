9dfe0e4f6a8fa3acdfb06dc9390d246434ddc19b
Iris014 <irisccallomamani@liceovvh.cl>
2026-07-13 16:13:09 -0400

    saved

--- STACK_JS/04_bucles/static/js/script.js
+++ STACK_JS/04_bucles/static/js/script.js
@@ -7,4 +7,28 @@ function contarhasta2() {
   }
 }
 
+function cuentaRegresiva() {
+for (let i = 10; i > 0; i--) {
+    alert(i);
+}
+}
+
+function recorrerLista() {
+  // Lista es un tipo de dato (array/lista/arreglos)
+// [] = ["Nombre", 4, true, [2, 4] ]
+/* 
+let nombre = "Carlos";
+alert (nombre);
+let nombres = ["Carlos", "Maria"]
+alert(nombres[0]) --> Imprime Carlos
+alert(nombres[1]) --> Imprime Maria
+alert(nombres[0].length); --> 0 (largo): 0
+alert(nombres.length); --> 2 (largo nombres)
+*/
+    let usuarios = ["Ana", "Luis", "María", "Simon", "Sofia"];
+
+  for (let i = 0; i < usuarios.length; i++) {
+      alert("Bienvenido, " + usuarios[i]);
+  }
+  }
 /* Ejemplo Bucle While */
