9e9559f71e8e4f0a90a02a82f795abf3031805b9
Iris014 <irisccallomamani@liceovvh.cl>
2026-07-15 09:30:39 -0400

    saved

---
+++ STACK_JS/07_arreglos/static/js/script.js
@@ -0,0 +1,14 @@
+console.log("Conexion con JS..")
+
+// Variables a lista
+let nombre1 = "Ana";
+let nombre2 = "Pedro";
+let nombre3 = "María";
+let nombre4 = "Carlos";
+
+// Transformar en array y mostrar
+let nombres = ["Ana", "Pedro", "María", "Carlos"]
+
+// Mostrar a Maria en una alerta
+alert(`María`);
+
