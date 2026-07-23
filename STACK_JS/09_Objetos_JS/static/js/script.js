d1bbdedd908237bae3b95abbbbdf41f9d74dfe58
Iris014 <irisccallomamani@liceovvh.cl>
2026-07-23 13:00:21 -0400

    saved

---
+++ STACK_JS/09_Objetos_JS/static/js/script.js
@@ -0,0 +1,54 @@
+console.log("Conexion con JS..")
+
+let hamburguesaEspecial = {
+    pan:"Pan brioche",
+    carne:"Pollo crujiente",
+    queso:"Suizo",
+    extras:[
+        "Lechuga",
+        "Pepinos",
+        "Miel"
+    ],
+    mostrarIngredientes:function(){
+        console.log("Pan:",this.pan);
+        console.log("Carne:",this.carne);
+        console.log("Queso:",this.queso);
+        console.log(
+            "Extras:",
+            this.extras.join(", ")
+        );
+    }
+};
+
+// Acceder al pan
+console.log(hamburguesaEspecial.pan)
+hamburguesaEspecial.mostrarIngredientes();
+
+// Objeto con método automóvil
+let auto = {
+    marca:"Toyota",
+    modelo:"Corolla",
+    año:2023,
+    encender:function(){
+        console.log(
+            "Encendiendo",
+            this.marca,
+            this.modelo
+        );
+    }
+    
+};
+auto.encender()
+
+// Objeto con método casa
+let casa = {
+    direccion:"Av. Siempre Viva 742",
+    habitaciones:4,
+    baños:2,
+    mostrarInformacion:function(){
+        console.log(
+            this.direccion
+        );
+    }
+};
+casa.mostrarInformacion()
