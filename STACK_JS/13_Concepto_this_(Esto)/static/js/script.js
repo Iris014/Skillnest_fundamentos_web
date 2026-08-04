fdda335da89b2b6d016ec5055fc481399a77958f
Iris014 <irisccallomamani@liceovvh.cl>
2026-08-04 15:01:45 -0400

    saved

--- STACK_JS/13_Concepto_this_(Esto)/static/js/script.js
+++ STACK_JS/13_Concepto_this_(Esto)/static/js/script.js
@@ -17,3 +17,39 @@ imagenes.forEach(function (imagen) {
         this.remove();
     });
 });
+
+let boton1 = document.querySelector(".boton1");
+boton1.addEventListener("click", function () {
+    if (this.innerText === "1") {
+        this.innerText = "2";
+    } else {
+        this.innerText = "1";
+    }
+});
+
+let boton2 = document.querySelector(".boton2");
+boton2.addEventListener("click", function () {
+    if (this.innerText === "2") {
+        this.innerText = "3";
+    } else {
+        this.innerText = "2";
+    }
+});
+
+let boton3 = document.querySelector(".boton3");
+boton3.addEventListener("click", function () {
+    if (this.innerText === "3") {
+        this.innerText = "4";
+    } else {
+        this.innerText = "3";
+    }
+});
+
+let desafio = document.getElementById("intercambiarTexto");
+desafio.addEventListener("click", function () {
+    if (this.innerText === "Haz clic en mí") {
+        this.innerText = "¡Texto cambiado!";
+    } else {
+        this.innerText = "Haz clic en mí";
+    }
+});
