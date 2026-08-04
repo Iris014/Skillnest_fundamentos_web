6baa09238aaa8d947bf9d6a2c60bbee3a4040fcd
Iris014 <irisccallomamani@liceovvh.cl>
2026-08-04 14:39:31 -0400

    saved

---
+++ STACK_JS/13_Concepto_this_(Esto)/static/js/script.js
@@ -0,0 +1,19 @@
+// Seleccionamos el botón por su id
+let boton = document.getElementById("cambiarTexto");
+
+// Agregamos un evento 'click' al botón
+boton.addEventListener("click", function () {
+    // Cambia el texto del botón usando `this`
+    this.innerText = "¡Texto cambiado!";
+});
+
+// Seleccionamos todas las imágenes con la clase 'imagenEliminar'
+let imagenes = document.querySelectorAll(".imagenEliminar");
+
+// Agregamos un evento 'click' a cada imagen
+imagenes.forEach(function (imagen) {
+    imagen.addEventListener("click", function () {
+        // Eliminamos la imagen al hacer clic
+        this.remove();
+    });
+});
