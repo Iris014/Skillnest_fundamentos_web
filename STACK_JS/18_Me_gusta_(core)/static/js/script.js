7d6c38ef1ae9fd76f5a62ebd98cceddd4e560046
Iris014 <irisccallomamani@liceovvh.cl>
2026-09-02 11:17:06 -0400

    saved

--- STACK_JS/18_Me_gusta_(core)/static/js/script.js
+++ STACK_JS/18_Me_gusta_(core)/static/js/script.js
@@ -1,10 +1,10 @@
 console.log("Conexión con js");
 
-const botonesLike = document.querySelectorAll(".like");
+const botonesLike = document.querySelectorAll(".seccion-like");
 botonesLike.forEach(function(boton) {
     boton.onclick = function() {
-        let cantidadLikes = parseInt(boton.innerText);
+        let cantidadLikes = parseInt(this.querySelector(".info-likes").innerText);
         cantidadLikes = cantidadLikes + 1;
-        boton.innerText = cantidadLikes + " Me gusta";
+        this.querySelector(".info-likes").innerText = cantidadLikes + " like(s)";
     };
 });
