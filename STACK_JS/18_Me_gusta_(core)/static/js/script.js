cfb88a7f539c9a965db27036e8a5ffb516af898b
Iris014 <irisccallomamani@liceovvh.cl>
2026-09-03 12:06:46 -0400

    saved

--- STACK_JS/18_Me_gusta_(core)/static/js/script.js
+++ STACK_JS/18_Me_gusta_(core)/static/js/script.js
@@ -1,28 +1,25 @@
 console.log("Conexión con js");
 
-const botonLike1 = document.querySelector("boton1");
-botonLike1.addEventListener("click", function () {
-    boton1.onclick = function() {
-        let cantidadLikes1 = parseInt(this.querySelector("boton1").innerText);
-        cantidadLikes1 = cantidadLikes1 + 1;
-        this.querySelector("boton1").innerText = cantidadLikes1 + " like(s)";
-    };
+const boton1 = document.getElementById("boton1");
+boton1.addEventListener("click", function () {
+    let infoLikes1 = this.previousElementSibling;
+    let cantidadLikes1 = parseInt(infoLikes1.innerText);
+    cantidadLikes1 = cantidadLikes1 + 1;
+    infoLikes1.innerText = cantidadLikes1 + " like(s)";
 });
 
-const botonLike2 = document.querySelector("boton2");
-botonLike2.addEventListener("click", function () {
-    boton2.onclick = function() {
-        let cantidadLikes2 = parseInt(this.querySelector("boton2").innerText);
-        cantidadLikes2 = cantidadLikes2 + 1;
-        this.querySelector("boton2").innerText = cantidadLikes2 + " like(s)";
-    };
+const boton2 = document.getElementById("boton2");
+boton2.addEventListener("click", function () {
+    let infoLikes2 = this.previousElementSibling;
+    let cantidadLikes2 = parseInt(infoLikes2.innerText);
+    cantidadLikes2 = cantidadLikes2 + 1;
+    infoLikes2.innerText = cantidadLikes2 + " like(s)";
 });
 
-const botonLike3 = document.querySelector("boton3");
-botonLike3.addEventListener("click", function () {
-    boton3.onclick = function() {
-        let cantidadLikes3 = parseInt(this.querySelector("boton3").innerText);
-        cantidadLikes3 = cantidadLikes3 + 1;
-        this.querySelector("boton3").innerText = cantidadLikes3 + " like(s)";
-    };
+const boton3 = document.getElementById("boton3");
+boton3.addEventListener("click", function () {
+    let infoLikes3 = this.previousElementSibling;
+    let cantidadLikes3 = parseInt(infoLikes3.innerText);
+    cantidadLikes3 = cantidadLikes3 + 1;
+    infoLikes3.innerText = cantidadLikes3 + " like(s)";
 });
