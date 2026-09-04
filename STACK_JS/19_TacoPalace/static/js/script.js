18a2867a34415fbb104a66c67e4702c35be183f6
Iris014 <irisccallomamani@liceovvh.cl>
2026-09-04 13:02:21 -0400

    saved

---
+++ STACK_JS/19_TacoPalace/static/js/script.js
@@ -0,0 +1,49 @@
+console.log("Conexión con js");
+
+let imagen = document.querySelector(".img");
+
+imagen.onclick = function() {
+    if (imagen.src = "static/images/Imágenes/comida-mexicana.jpg"){
+        imagen.src = "static/images/Imágenes/comida-mexicana2.jpg";
+    } else if (imagen.src = "static/images/Imágenes/comida-mexicana2.jpg"){
+        imagen.src = "static/images/Imágenes/comida-mexicana2.jpg";
+    } 
+};
+
+/*
+            <div class="busqueda">
+                <input type="text" id="barra-busqueda" name="email" placeholder="Email">
+                <button class="login">Login</button>
+            </div>
+*/
+
+let correo = document.getElementById("barra-busqueda");
+
+let login = document.querySelector(".login");
+login.onclick = function() {
+    alert(`Bienvenido/a ${correo.innerText}`)
+}
+
+const boton1 = document.getElementById("boton1");
+boton1.addEventListener("click", function () {
+    let infoLikes1 = this.previousElementSibling;
+    let cantidadLikes1 = parseInt(infoLikes1.innerText);
+    cantidadLikes1 = cantidadLikes1 + 1;
+    infoLikes1.innerText = cantidadLikes1 + " like(s)";
+});
+
+const boton2 = document.getElementById("boton2");
+boton2.addEventListener("click", function () {
+    let infoLikes2 = this.previousElementSibling;
+    let cantidadLikes2 = parseInt(infoLikes2.innerText);
+    cantidadLikes2 = cantidadLikes2 + 1;
+    infoLikes2.innerText = cantidadLikes2 + " like(s)";
+});
+
+const boton3 = document.getElementById("boton3");
+boton3.addEventListener("click", function () {
+    let infoLikes3 = this.previousElementSibling;
+    let cantidadLikes3 = parseInt(infoLikes3.innerText);
+    cantidadLikes3 = cantidadLikes3 + 1;
+    infoLikes3.innerText = cantidadLikes3 + " like(s)";
+});
