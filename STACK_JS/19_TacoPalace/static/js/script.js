2edd91d0ec7b7c27235cecd6fa75b628a00525de
Iris014 <irisccallomamani@liceovvh.cl>
2026-09-08 15:23:01 -0300

    saved

--- STACK_JS/19_TacoPalace/static/js/script.js
+++ STACK_JS/19_TacoPalace/static/js/script.js
@@ -25,34 +25,26 @@ login.onclick = function() {
 }
 
 
+const Likes = document.querySelectorAll(".info-likes");
+
 const boton1 = document.getElementById("boton1");
 boton1.addEventListener("click", function () {
-    let infoLikes1 = this.previousElementSibling;
+    let infoLikes1 = Likes[0];
     let cantidadLikes1 = parseInt(infoLikes1.innerText);
     cantidadLikes1 = cantidadLikes1 + 1;
     infoLikes1.innerText = cantidadLikes1 + " like(s)";
 });
-
 const boton2 = document.getElementById("boton2");
 boton2.addEventListener("click", function () {
-    let infoLikes2 = this.previousElementSibling;
+    let infoLikes2 = Likes[1];
     let cantidadLikes2 = parseInt(infoLikes2.innerText);
     cantidadLikes2 = cantidadLikes2 + 1;
     infoLikes2.innerText = cantidadLikes2 + " like(s)";
 });
-
 const boton3 = document.getElementById("boton3");
 boton3.addEventListener("click", function () {
-    let infoLikes3 = this.previousElementSibling;
+    let infoLikes3 = Likes[2];
     let cantidadLikes3 = parseInt(infoLikes3.innerText);
     cantidadLikes3 = cantidadLikes3 + 1;
     infoLikes3.innerText = cantidadLikes3 + " like(s)";
-});
-
-const boton4 = document.getElementById("boton4");
-boton4.addEventListener("click", function () {
-    let infoLikes4 = this.previousElementSibling;
-    let cantidadLikes4 = parseInt(infoLikes4.innerText);
-    cantidadLikes4 = cantidadLikes4 + 1;
-    infoLikes4.innerText = cantidadLikes4 + " like(s)";
 });
